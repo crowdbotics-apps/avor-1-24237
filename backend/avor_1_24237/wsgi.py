abc04122554352f7e9975e53c58ecd6f5bf49343
Crowdbotics <team@crowdbotics.com>
2021-02-04 02:23:36 +0000

    Update models

--- backend/avor_1_24237/wsgi.py
+++ backend/avor_1_24237/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avor_1_24237.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "avor_1_24237.settings")
 
 application = get_wsgi_application()
